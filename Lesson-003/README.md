d690af2a1a0ddacd5434631442d034b0cf5b3e3b
kamil-kalandyk <kamil.kalandyk@codewise.com>
2017-04-24 16:06:52 +0200

    HelloWorldController

--- Lesson-003/README.md
+++ Lesson-003/README.md
@@ -1,1 +1,5 @@
-# Lesson 3 - Hello World from Server 
+# Lesson 3 - Hello World from Server
+ 
+ ## 1. Add Hello World Controller
+ 
+ Add new java file [HelloWorldController](demo/src/main/java/pl/server/rest/HelloWorldController.java) in your **demo** project.
---
+++ Lesson-003/demo/src/main/java/pl/server/rest/HelloWorldController.java
@@ -0,0 +1,14 @@
+package pl.server.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET, produces = "application/json")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
